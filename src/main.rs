2c1eb257310c21c4b05bd39680e0587a5fce5560
devinMcArthur <assortedgerm@gmail.com>
2023-06-24 22:17:30 -0600

    Finish Chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::net::TcpListener;
 
 use rust_to_prod::{configuration::get_configuration, startup::run};
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     // We have removed the hard-coded `8000` - it's now coming from our settings
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
