6cd4cd1d2d7e06de81d5d7e5c8007a0e2d14d189
devinMcArthur <assortedgerm@gmail.com>
2023-07-18 09:36:55 -0600

    Setup production database

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use rust_to_prod::{
     startup::run,
     telemetry::{get_subscriber, init_subscriber},
 };
-use secrecy::ExposeSecret;
 use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
@@ -17,8 +16,7 @@ async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(&configuration.database.connection_string().expose_secret())
-        .expect("Failed to connect to Postgres.");
+        .connect_lazy_with(configuration.database.with_db());
 
     // We have removed the hard-coded `8000` - it's now coming from our settings
     let address = format!(
